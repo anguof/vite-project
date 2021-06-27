bb071bbde74cec93ef3f7ea289b9123a97a6235f
anguo <anguo@surpath.net>
2021-06-27 12:23:50 +0800

    setState sync and async

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+
+// function App() {
+//   const [count, setCount] = useState(1);
+
+//   useEffect(() => {
+//     // setTimeout(() => {
+//     //   setCount(2);
+//     //   console.log(count);
+//     // }, 0);
+
+//     setCount(2);
+//     console.log(count);
+//   }, []);
+
+//   console.log(count);
+
+//   return <p>{count}</p>;
+// }
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,17 +31,21 @@ class App extends Component {
     // this.setState({
     //   count: this.state.count + 1,
     // });
+    // this.setState({
+    //   count: this.state.count + 1,
+    // }); //异步更新：触发额外渲染，在浏览器更新屏幕前
     setTimeout(() => {
       this.setState({
         count: this.state.count + 1,
       });
       console.log(this.state.count);
-    }, 0);
+    }, 0); //跳过react机制：同步更新
     // console.log(this.state.count);
   }
 
   render() {
     const { gender, count } = this.state;
+    console.log(count);
     return (
       <div>
         <p>{gender}</p>
